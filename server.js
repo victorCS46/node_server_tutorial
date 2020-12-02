71e4d5b1ffaef6b0b8510bda738a6e4908dde408
Victor Cruz <victorcruzsuero@gmail.com>
2020-12-02 10:29:55 -0400

    Adding start script in package.json

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
-const http = require('http')
 const axios = require('axios')
-const hbs = require('hbs')
 const express = require('express')
 const app = express()
- 
+const hbs = require('hbs')
+require('./hbs/helpers')
+
+const port = process.env.PORT || 3000
+
 app.use(express.static(`${__dirname}/public`))
 
 hbs.registerPartials(`${__dirname}/views/partials`)
@@ -11,7 +13,7 @@ app.set('view engine', 'hbs')
 
 app.get('/', (req, res) => {
   res.render('home', {
-    name: 'victor'
+    name: 'viewer'
   })
 })
 
@@ -19,4 +21,4 @@ app.get('/about', (req, res) => {
   res.render('about')
 })
  
-app.listen(3000, () => console.log('listen: http://127.0.0.1:3000'))
+app.listen(port, () => console.log(`listen: http://127.0.0.1:${port}`))
